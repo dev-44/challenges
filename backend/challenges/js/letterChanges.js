451110ddf38fb4f1ec5e7b589e76af190dee7810
plug47 <oarmoa44@gmail.com>
2022-07-23 16:48:42 -0400

    New js challenges

---
+++ backend/challenges/js/letterChanges.js
@@ -0,0 +1,39 @@
+/*
+LETTER CHANGES - String Manipulation
+
+Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. 
+Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). 
+Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string. 
+
+Examples
+---------------------
+Input: "hello*3"
+Output: Ifmmp*3
+
+Input: "fun times!"
+Output: gvO Ujnft! 
+*/
+
+function LetterChanges(str) { 
+
+   // code goes here
+   let nextL = []
+   let alphabet = "abcdefghijklmnopqrstuvwxyz"
+   
+   for(let i=0; i < str.length; i++) {
+     if(alphabet.indexOf(str[i]) !== -1) {
+       var index = alphabet.indexOf(str[i])
+       nextL.push(alphabet[index + 1])
+     } else {
+       nextL.push(str[i])
+     }
+   }
+ 
+   nextL = nextL.join('')
+ 
+   let changed = nextL.replace(/[aeiou]/g, (v) => {
+     return v.toUpperCase()
+   })
+ 
+   return changed
+ }
---
+++ backend/challenges/js/userNameValidation.js
@@ -0,0 +1,43 @@
+/*
+Have the function CodelandUsernameValidation(str) take the str parameter being passed and determine if the string is a valid username according to the following rules:
+
+1. The username is between 4 and 25 characters.
+2. It must start with a letter.
+3. It can only contain letters, numbers, and the underscore character.
+4. It cannot end with an underscore character.
+
+If the username is valid then your program should return the string true, otherwise return the string false.
+*/
+
+function CodelandUsernameValidation(str) { 
+   
+   // code goes here
+
+   if(str.length < 4 || str.length > 25) {
+      return false 
+   }
+   
+   var first = str[0]
+   var last = str[str.length-1]
+   console.log(first)
+   console.log(last)
+
+ 
+   if(/[a-z]/i.test(first) === false || /_/.test(last)) {
+     return false
+   }
+ 
+   if(/[a-zA-Z0-9_]/g.test(str)) {
+     return true
+   } else {
+     return false
+   }
+
+ 
+ }
+
+ console.log(CodelandUsernameValidation("__bbbbbbb"))
+ console.log(CodelandUsernameValidation("b3333434_"))
+ console.log(CodelandUsernameValidation("oooooooooooooooooo________a"))
+ console.log(CodelandUsernameValidation("123abc444"))
+ console.log(CodelandUsernameValidation("a______b_________555555555555aaaa"))
