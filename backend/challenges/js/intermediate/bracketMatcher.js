f6dc78635b25bfd13b1d1689ec37841564e0b478
Oscar Armoa <oarmoa44@gmail.com>
2023-04-16 18:34:07 -0400

    new challenges added

--- backend/challenges/js/intermediate/bracketMatcher.js
+++ backend/challenges/js/intermediate/bracketMatcher.js
@@ -7,31 +7,31 @@ Only "(" and ")" will be used as brackets. If str contains no brackets return 1.
 
 function BracketMatcher(str) { 
 
-   // code goes here 
-   var open = 0
-   var close = 0
-   
-   for(var i=0; i < str.length; i++) {
-     if(str[i] === '('){
-       if(str[i] == str[i+1]) {
-         return 0
-       }
-       open++
-     }
-     if(str[i] === ')'){
-       close++
-     }
-   }
- 
-   if(open === close){
-     return 1
-   } else {
-     return 0
-   }
-   //return str; 
-   
- 
- }
+  // code goes here 
+  var open = 0
+  var close = 0
+  
+  for(var i=0; i < str.length; i++) {
+    if(str[i] === '('){
+      if(str[i] == str[i+1]) {
+        return 0
+      }
+      open++
+    }
+    if(str[i] === ')'){
+      close++
+    }
+  }
+
+  if(open === close){
+    return 1
+  } else {
+    return 0
+  }
+  //return str; 
+  
+
+}
     
  // keep this function call here 
  console.log(BracketMatcher(readline()));
